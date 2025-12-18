97e4074ca79dfd724c0e190eaa7519fa4f498fe0
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2025-12-18 16:26:26 +0100

    Use the Github hosted version of Tchap dependencies (matrix-rust-sdk and compound-design-tokens)

--- compound-ios/Package.swift
+++ compound-ios/Package.swift
@@ -9,8 +9,10 @@ let package = Package(
         .library(name: "Compound", targets: ["Compound"])
     ],
     dependencies: [
-//        .package(url: "https://github.com/tchapgouv/compound-design-tokens", branch: "main"),
-        .package(path: "../../tchap-x-compound/compound-design-tokens"),
+// Use the Github hosted version of Tchap Compound-design--tokens
+       .package(url: "https://github.com/tchapgouv/compound-design-tokens", branch: "angelo-uikit-ev2"),
+// Use the local version of Tchap Compound-design--tokens
+//        .package(path: "../../tchap-x-compound/compound-design-tokens"),
         .package(url: "https://github.com/siteline/SwiftUI-Introspect", from: "26.0.0"),
         .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols", from: "7.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", exact: "1.18.3")
