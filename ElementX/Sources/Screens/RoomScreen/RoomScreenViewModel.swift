45b625e7fe5cc2b9028e897a3defa8b4624061bb
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-02-03 15:34:54 +0100

    Update binding without needing powerLevels

--- ElementX/Sources/Screens/RoomScreen/RoomScreenViewModel.swift
+++ ElementX/Sources/Screens/RoomScreen/RoomScreenViewModel.swift
@@ -341,16 +341,16 @@ class RoomScreenViewModel: RoomScreenViewModelType, RoomScreenViewModelProtocol 
             state.canAcceptKnocks = powerLevels.canOwnUserInvite()
             state.canDeclineKnocks = powerLevels.canOwnUserKick()
             state.canBan = powerLevels.canOwnUserBan()
-        
-            // Tchap: fill room properties
-            state.bindings.isEncrypted = roomProxy.details.isEncrypted
-            state.bindings.isPublic = roomProxy.details.isPublic
-            // Tchap: read the `external access` value in the `accessRules` of the room.
-            // Used to display "open to external users" badge.
-            state.bindings.accessRule = roomProxy.details.accessRule
-            state.bindings.canDisplayPublicBadge = roomProxy.details.canDisplayPublicBadge
-            state.bindings.roomAvatar = roomProxy.details.avatar
         }
+        
+        // Tchap: fill room properties
+        state.bindings.isEncrypted = roomProxy.details.isEncrypted
+        state.bindings.isPublic = roomProxy.details.isPublic
+        // Tchap: read the `external access` value in the `accessRules` of the room.
+        // Used to display "open to external users" badge.
+        state.bindings.accessRule = roomProxy.details.accessRule
+        state.bindings.canDisplayPublicBadge = roomProxy.details.canDisplayPublicBadge
+        state.bindings.roomAvatar = roomProxy.details.avatar
     }
     
     private func setupPinnedEventsTimelineItemProviderIfNeeded() {
