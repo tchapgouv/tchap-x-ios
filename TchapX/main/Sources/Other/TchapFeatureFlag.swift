dd06193c605e82002583d62b369635c7a28d09e9
Anaïs Payet <anais.payet.ext@numerique.gouv.fr>
2026-03-26 16:05:23 +0100

    Allow unencryptedPrivateRoom in dev

--- TchapX/main/Sources/Other/TchapFeatureFlag.swift
+++ TchapX/main/Sources/Other/TchapFeatureFlag.swift
@@ -126,7 +126,7 @@ extension TchapFeatureFlag {
         // CertificatePinning feature flag pinning is disabled in Development version (which uses Let's Encrypt certificates).
         // See `TchapX/target.yml` file to know how to generate certificate pinning.
         static let certificatePinning = TchapFeatureFlag(allowedInstances: [])
-        static let unencryptedPrivateRoom = TchapFeatureFlag(allowedInstances: [])
+        static let unencryptedPrivateRoom = TchapFeatureFlag(allowedInstances: [.all])
         // enableMAS feature flag can only be activated for .all or none because it is used before any activated session.
         static let enableMAS = TchapFeatureFlag(allowedInstances: [.all])
         #endif
