87ee9661129f5bdfba0128ab6ecfbe181de8fcbb
Anaïs Payet <anais.payet.ext@numerique.gouv.fr>
2026-09-11 16:40:07 +0200

    Tchap: fix typo

--- ElementX/Sources/Generated/Strings.swift
+++ ElementX/Sources/Generated/Strings.swift
@@ -3234,7 +3234,7 @@ internal nonisolated enum L10n {
   }
   /// This is the beginning of this conversation.
   internal static var screenRoomTimelineBeginningOfRoomNoName: String { return L10n.tr("Localizable", "screen_room_timeline_beginning_of_room_no_name") }
-  /// Unsupported call. Ask if the caller can use the new Element X app.
+  /// Unsupported call. Ask if the caller can use the new Tchap app.
   internal static var screenRoomTimelineLegacyCall: String { return L10n.tr("Localizable", "screen_room_timeline_legacy_call") }
   /// Show less
   internal static var screenRoomTimelineLessReactions: String { return L10n.tr("Localizable", "screen_room_timeline_less_reactions") }
